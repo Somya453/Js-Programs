3b4d0553a8390dcc99ceed2cef9b01bc6bb4237a
somya <somyasharma0488@gmail.com>
2025-03-04 21:09:47 +0530

    Add files via upload
    Signed-off-by: somya <somyasharma0488@gmail.com>

---
+++ DOM.js
@@ -0,0 +1,34 @@
+//DOM-Document Object Model
+// // const box1=document.getElementById('box1');
+// // box1.style.backgroundColor="REd";
+
+
+// const container=document.getElementById('container');
+// container.appendChild(newParagh);
+
+
+function color(box1) {
+    
+}
+
+const hover=()=>{
+    console.log();
+    
+}
+
+
+function keyPressEvent() {
+    console.log('Key was pressed');
+}
+    
+function keyUpEvent() {
+    console.log('Key up was pressed');
+    
+    
+}
+
+function keyDownEvent() {
+    console.log('Key down was pressed');
+    
+    
+}
