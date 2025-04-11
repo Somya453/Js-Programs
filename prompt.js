5be53da3e5feff9503da61291fe2981c36435cce
somya <somyasharma0488@gmail.com>
2025-04-11 10:10:24 +0530

    main

--- prompt.js
+++ prompt.js
@@ -1,11 +1,11 @@
-// let num=prompt("Enter a number");
+let num=prompt("Enter a number");
 
-// if (num%5===0) {
-//     console.log(num, " is divisible by 5");
+if (num%5===0) {
+    console.log(num, " is divisible by 5");
     
-// }else{
-//     console.log(num, " is not divisible by 5");
+}else{
+    console.log(num, " is not divisible by 5");
 
     
-// }
+}
 
