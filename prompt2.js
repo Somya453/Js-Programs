3264f3e26d50d5b45ae932139c8167e4f7ff3102
somya <somyasharma0488@gmail.com>
2025-04-12 22:04:19 +0530

    main

---
+++ prompt2.js
@@ -0,0 +1,8 @@
+let name=prompt("Enter your name");
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet(name));
+// The above code prompts the user for their name and then greets them with a message.
