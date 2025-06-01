54897e795f87d030103df581f1c44fb72505da24
somya <somyasharma0488@gmail.com>
2025-06-01 20:07:20 +0530

    main

---
+++ callfn.js
@@ -0,0 +1,14 @@
+const greet=(name, callback)=>{
+    console.log("Hello"+' '+name+'!');
+    callback();
+    
+}
+
+// Callback function
+// const call=(greet, name) => {
+//     greet(name);
+// }
+
+greet("John", () => {
+    console.log("This is a callback function.");
+});
