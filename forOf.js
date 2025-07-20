553524d0753bcb28ed596c7713696c28ad7b2a2c
somya <somyasharma0488@gmail.com>
2025-07-20 23:35:38 +0530

    main+

--- forOf.js
+++ forOf.js
@@ -4,3 +4,9 @@ for (let letter of name) {
   console.log(letter);
 }
 
+const boroughs = ["brooklyn", "manhattan", "the bronx", "queens", "staten island"];
+
+for (let borough of boroughs) {
+  console.log(borough);
+}
+
