8619e6db0b85e9280b02485f76bde8d988756306
somyaa <somyasharma0488@gmail.com>
2025-07-20 12:39:25 +0530

    Create ES6.js
    Signed-off-by: somyaa <somyasharma0488@gmail.com>

---
+++ ES6.js
@@ -0,0 +1,26 @@
+// 
+// function greet(name) {
+//   return "Hello, " + name + "!";
+// }
+
+// console.log(greet("Nimi"));
+// // Output: Hello, Nimi!
+
+// const greet = (name) => {
+//   return "Hello, " + name + "!";
+// };
+
+// console.log(greet("Asir"));
+// Output: Hello, Asir!
+
+
+// console.log(dadJoke("I used to play piano by ear, but now I use my hands."));
+
+// const dadJoke = (text) => ("hi");
+
+
+function joke(dadJoke) {
+  return dadJoke;
+}
+
+console.log(joke("I used to play piano by ear, but now I use my hands."));
