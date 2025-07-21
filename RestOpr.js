f7535e236b5dad1371f7fbbd73180c4dbd59b678
somya <somyasharma0488@gmail.com>
2025-07-21 15:49:48 +0530

    main

--- RestOpr.js
+++ RestOpr.js
@@ -10,3 +10,25 @@ console.log(packedGroceries);
 
 const morePackedGroceries = packGroceries("bread", "butter", "cheese");
 console.log(morePackedGroceries);
+
+
+// // # Rest Operator with Function Parameters
+// const calculateTotal = (basePrice, taxRate, ...additionalFees) => {
+//     let total = basePrice + (basePrice * taxRate);
+//     for (let fee of additionalFees) {
+//         total += fee;
+//     }
+//     return total;
+// }
+
+
+// example:
+const planVacation = (...destinations) => {
+    return destinations;
+}
+
+const destinationOne = planVacation("Paris", "Tokyo", "New York", "Sydney");
+console.log(destinationOne);
+
+const destinationTwo = planVacation("London", "Berlin");
+console.log(destinationTwo);
