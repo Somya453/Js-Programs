79b7d354ff9898b456d8b993b03450a170097a59
somyaa <somyasharma0488@gmail.com>
2025-08-28 22:58:06 +0530

    Update DOM.js
    Signed-off-by: somyaa <somyasharma0488@gmail.com>

--- DOM.js
+++ DOM.js
@@ -7,13 +7,11 @@
 // container.appendChild(newParagh);
 
 
-function color(box1) {
-    
+function color(box1) {  
 }
 
 const hover=()=>{
     console.log();
-    
 }
 
 
@@ -23,12 +21,44 @@ function keyPressEvent() {
     
 function keyUpEvent() {
     console.log('Key up was pressed');
-    
-    
 }
 
 function keyDownEvent() {
     console.log('Key down was pressed');
-    
-    
 }
+
+
+// Get the box element
+const box = document.getElementById("box");
+
+box.style.width = "200px";
+box.style.border = "2px solid black";
+
+// Change the background color of the box
+box.style.backgroundColor = "lightgreen";
+
+// Add some text to the box
+box.textContent = "Hello, DOM!";
+
+let a = document.querySelector("h1"); 
+a.innerHTML = "New Heading";              //to select <h1> heading
+
+// h1.innerHTML = "Another Heading(Here we can change the heading or p.)";
+console.log(a);
+
+a.style.backgroundColor = "lightblue"; /////For styling in CSS
+
+
+
+////Event Listener
+let h1 = document.querySelector("h1");
+h1.addEventListener("click", function() {
+    console.log(h1);
+    alert("Heading clicked!");   
+});
+
+//Event Listener in  css styling
+let b = document.getElementById("box");
+b.addEventListener("mouseover", function() {
+    b.style.backgroundColor = "yellow";   //when mouse is over the box
+});
