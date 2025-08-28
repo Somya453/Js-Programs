3b2616009caa3d7c296a02d6e4e22b24d5d68689
somyaa <somyasharma0488@gmail.com>
2025-08-28 23:00:29 +0530

    Create bitwise.js
    Signed-off-by: somyaa <somyasharma0488@gmail.com>

---
+++ bitwise.js
@@ -0,0 +1,31 @@
+//Bitwise Operators
+
+console.log(8 | 11);
+console.log(26 & 15);
+console.log(26 | 15);
+console.log(~3);
+
+let y = 10;
+console.log(~y);
+
+let a = b = c = 15;
+console.log(a);
+console.log(b);
+console.log(c);
+
+let num1=55;
+let result = (num1 % 2 == 0) ? "Even" : "Odd";
+console.log(result);
+
+let num2 = 2;
+let result2 = (num2 >= 10 && num2 <= 0) ? true : false;
+console.log(result2);
+
+
+let username = "JohnDoe";
+let password = "SecurePassword123";
+
+let result3 = (username === "JohnDoe" && password === "SecurePassword123") ? "Login successful" : "Login failed";
+console.log(result3);
+
+console.log(2/0);
